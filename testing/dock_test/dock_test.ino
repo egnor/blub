1180a67465e3bec13d8bfb08c94a264eb3f507ca
Dan Egnor <egnor@ofb.net>
2025-11-25 18:47:39 -0800

    updated dock test

--- testing/dock_test/dock_test.ino
+++ testing/dock_test/dock_test.ino
@@ -8,19 +8,25 @@ static const OkLoggingContext OK_CONTEXT("display_test");
 extern char const* const ok_logging_config = "*=DETAIL";
 
 void loop() {
-  OK_NOTE("LOOP");
-  ok_dock_layout->line_printf(0, "\f9Hello World!");
-  ok_dock_layout->line_printf(
-    1, "\f9[%d] [%d] [%d]",
+  uint8_t const b[] = {
     ok_dock_button(0),
     ok_dock_button(1),
     ok_dock_button(2)
-  );
-  delay(20);
+  };
+  OK_NOTE("LOOP, layout=%p, b=%d/%d/%d", ok_dock_layout, b[0], b[1], b[2]);
+  if (ok_dock_layout) {
+    ok_dock_layout->line_printf(0, "\f11\bHello Dock!");
+    ok_dock_layout->line_printf(1, "\f9Buttons %d %d %d", b[0], b[1], b[2]);
+  }
+  delay(300);
 }
 
 void setup() {
   ok_serial_begin();
   OK_NOTE("SETUP");
+  for (int i = 5; i >= 0; --i) {
+    OK_NOTE("Starting in %d...", i);
+    delay(1000);
+  }
   OK_FATAL_IF(!ok_dock_init_feather_v8());
 }
